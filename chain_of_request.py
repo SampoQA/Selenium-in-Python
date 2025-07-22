dfa8f0cb4235426f4904337b23fb430be62ba340
SampoQA <tertusampo@gmail.com>
2025-07-22 21:45:36 +0300

    new i'm adding a new feature to handle chains of requests

--- chain_of_request.py
+++ chain_of_request.py
@@ -7,13 +7,27 @@ driver = webdriver.Chrome()
 action = ActionChains(driver) # Создаем обьект action
 
 LEFT_CLICK_BUTTON = ("xpath", "//button[@id='leftClick']")
+DOUBLE_CLICK_BUTTON = ("xpath", "//button[@id='doubleClick']")
+RIGHT_CLICK_BUTTON = ("xpath", "//button[@id='rightClick']")
+HOVER_BUTTON = ("xpath", "//button[@id='colorChangeOnHover']")
 
 driver.get("https://testkru.com/Elements/Buttons")
 
 left_button= driver.find_element(*LEFT_CLICK_BUTTON)
+double_button = driver.find_element(*DOUBLE_CLICK_BUTTON)
+right_button = driver.find_element(*RIGHT_CLICK_BUTTON)
+hover_button = driver.find_element(*HOVER_BUTTON)
 
 time.sleep(3)  # Задержка для наблюдения результата
+action.click(left_button).pause(3).double_click(double_button).context_click(right_button).perform()  # Цепочка действий
 
-action.click(left_button).perform()  # Важно использовать метод perform() для выполнения действия завершения цепочки действий
+# action.double_click(double_button).perform()  # Двойной клик по кнопке
+# time.sleep(5)  # Задержка для наблюдения результата
 
-time.sleep(3)  # Задержка для наблюдения результата
+# action.context_click(right_button).perform()  # Правый клик по кнопке
+# time.sleep(5)  # Задержка для наблюдения результата
+
+# action.click(left_button).perform()  # Левый клик по кнопке
+time.sleep(5)  # Задержка для наблюдения результата
+action.move_to_element(hover_button).perform()  # Наведение курсора на кнопку
+time.sleep(5)  # Задержка для наблюдения результата
